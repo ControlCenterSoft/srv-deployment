5b19f216ddd8ebd9c89a1ae2d519a13219f0eced
filosoff31 <40738562+filosoff31@users.noreply.github.com>
2026-08-19 13:58:06 +0300

    Register strict DHCP reservation validation

--- app/wsgi.py
+++ app/wsgi.py
@@ -18,6 +18,7 @@ import release_111_safety
 import release_111_notifications
 import release_111_services
 import release_111_dependencies
+import release_111_dhcp
 import release_111_services_assets
 import release_111_auth
 
@@ -57,6 +58,7 @@ release_111_safety.register(app, main)
 release_111_notifications.register(app, main)
 release_111_services.register(app, main)
 release_111_dependencies.register(app, main)
+release_111_dhcp.register(app, main)
 release_111_services_assets.register(app, main)
 release_111_auth.register(app, main)
 
